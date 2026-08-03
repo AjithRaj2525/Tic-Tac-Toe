81e01e8277dd3d6ed0565af14d6ee3d524cb0008
Ajith Raj P <ajithrajp25@gmail.com>
2026-08-03 16:44:37 +0530

    Store game board in React state

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 import Square from "./components/Square";
 
 function App() {
+
+  const [board, setBoard] = useState([
+    "", "", "",
+    "", "", "",
+    "", "", ""
+  ]);
+
   return (
     <div className="container">
       <h1>Tic Tac Toe</h1>
 
       <div className="board">
-        <Square value="X" />
-        <Square value="O" />
-        <Square value="X" />
+        <Square value={board[0]} />
+        <Square value={board[1]} />
+        <Square value={board[2]} />
 
-        <Square value="O" />
-        <Square value="X" />
-        <Square value="O" />
+        <Square value={board[3]} />
+        <Square value={board[4]} />
+        <Square value={board[5]} />
 
-        <Square value="X" />
-        <Square value="O" />
-        <Square value="X" />
+        <Square value={board[6]} />
+        <Square value={board[7]} />
+        <Square value={board[8]} />
       </div>
     </div>
   );
